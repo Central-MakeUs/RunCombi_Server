15c4a33e0b73b713480258f6c075f729df3634db
youngho3358 <youngho3358@gmail.com>
2025-07-13 22:33:24 +0900

    feat: Amazon S3 bucket 연동
    - 파일 업로드, 삭제 기능 구현
    - S3 버킷 접근 권한 설정 및 환경변수 적용
    - S3 의존성 추가
    - 커스텀 예외 추가

--- src/main/java/com/runcombi/server/global/exception/code/CustomErrorList.java
+++ src/main/java/com/runcombi/server/global/exception/code/CustomErrorList.java
@@ -18,7 +18,11 @@ public enum CustomErrorList implements CustomErrorCode{
     KAKAO_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "KAKAO0003", "만료된 카카오 인증 토큰입니다."),
 
     // Member
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER0001", "사용자가 존재하지 않습니다.");
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER0001", "사용자가 존재하지 않습니다."),
+
+    // S3 이미지 업로드 에러
+    S3_IMAGE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "BUCKET0001", "이미지 업로드에 실패했습니다."),
+    S3_IMAGE_DELETE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "BUCKET0002", "기존 이미지 삭제에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
